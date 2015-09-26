18e2428011a3be3decc5137029a1840ee4becc3c
JC/BC <analytalica@gmail.com>
2015-09-26 17:55:22 -0500

    Added some more keywords

---
+++ samples/sample1.js
@@ -0,0 +1,18 @@
+for
+    (
+        var n = 1;
+        n <= 100;
+        n = n + 1
+    )
+{
+    var out = "";
+    if
+        (! (n % 3))
+        out = out + "Fizz";
+    if
+        (! (n % 5))
+        out = out + "Buzz";
+    console.log (out
+        ? out
+        : n);
+}
