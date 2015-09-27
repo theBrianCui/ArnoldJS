6c29ef4794549783a229a5f18e623ead149b341e
JC/BC <analytalica@gmail.com>
2015-09-27 15:33:41 -0500

    Switched from node-beautify to js-beautify

--- samples/fizzbuzz.js
+++ samples/fizzbuzz.js
@@ -1,9 +1,10 @@
 for (
-var n = 1;
-n <= 100;
-n = n + 1) {
+    var n = 1; n <= 100; n = n + 1
+) {
     var out = "";
-    if (!(n % 3)) out = out + "Fizz";
-    if (!(n % 5)) out = out + "Buzz";
+    if (!(n % 3))
+        out = out + "Fizz";
+    if (!(n % 5))
+        out = out + "Buzz";
     console.log(out ? out : n);
 }
--- samples/mergesort.js
+++ samples/mergesort.js
@@ -1,13 +1,18 @@
 var array = [2, 5, 1, 6, 8, 12, -4, 2];
 
 function mergesort(input) {
-    if (input.length > 1) return merge(
-    mergesort(input.slice(0, input.length / 2)), mergesort(input.slice(input.length / 2)));
+    if (input.length > 1)
+        return merge(
+            mergesort(input.slice(0, input
+                .length / 2)),
+            mergesort(input.slice(input
+                .length / 2)));
     return input
 }
 
 function merge(
-left, right) {
+    left, right
+) {
     var result = [];
     var leftIndex = 0;
     var rightIndex = 0;
@@ -15,20 +20,20 @@ left, right) {
     while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
             result.push(left[leftIndex++]);
-        }
-        else {
+        } else {
             result.push(right[rightIndex++]);
         }
     }
 
     if (leftIndex === left.length) {
         result = result.concat(right.slice(rightIndex));
-    }
-    else if (rightIndex === right.length) {
+    } else
+    if (rightIndex === right.length) {
         result = result.concat(left.slice(leftIndex));
     }
     return result;
 }
 
 console.log(
-mergesort(array));
+    mergesort(array)
+);
